cfd445e29e3bf7d893f6cc1a8794011568dcd122
maria-mamede <maria.mamede@mercadolivre.com>
2022-07-14 10:30:24 -0300

    iniciando testes

--- src/test/java/com/meli/desafioquality/service/CalculatePropertyTest.java
+++ src/test/java/com/meli/desafioquality/service/CalculatePropertyTest.java
@@ -1,4 +1,36 @@
+package com.meli.desafioquality.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatePropertyTest {
-  
+
+    @Test
+    void createProperty() {
+    }
+
+    @Test
+    void calculateSqrFtgProp() {
+    }
+
+    @Test
+    void calculatePrice() {
+    }
+
+    @Test
+    void calculateSqrFtgRoom() {
+    }
+
+    @Test
+    void largestRoom() {
+    }
+
+    @Test
+    void getAllDistricts() {
+    }
+
+    @Test
+    void createDistrict() {
+    }
 }
