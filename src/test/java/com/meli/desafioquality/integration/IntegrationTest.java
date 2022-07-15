2ffec10e3b75b8498d93886229731d9bae1b9681
Danielen <danielen.cestari@mercadolivre.com>
2022-07-15 14:51:28 -0300

    teste integração RegisterProperty

--- src/test/java/com/meli/desafioquality/integration/IntegrationTest.java
+++ src/test/java/com/meli/desafioquality/integration/IntegrationTest.java
@@ -1,6 +1,8 @@
 package com.meli.desafioquality.integration;
 
+import com.meli.desafioquality.dto.PropertyRequest;
 import com.meli.desafioquality.model.District;
+import com.meli.desafioquality.model.Property;
 import com.meli.desafioquality.repository.DistrictRepo;
 import com.meli.desafioquality.repository.PropertyRepo;
 import com.meli.desafioquality.util.TestUtil;
@@ -21,7 +23,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PropertiesIntegrationTest {
+public class IntegrationTest {
 
     @LocalServerPort
     private int port;
@@ -61,7 +63,25 @@ public class PropertiesIntegrationTest {
     }
 
     @Test
+    @DisplayName("Testa quando a lista de bairros está vazia")
     public void getAllDistricts() {
+        List<District> allDistricts = TestUtil.getAllDistricts();
+        String baseUrl = "http://localhost:" + port + "/district";
+
+        ResponseEntity<District[]> districtsReturn =
+                testRestTemplate.exchange(baseUrl,
+                        HttpMethod.GET, null, District[].class);
+
+        District[] districtBody = districtsReturn.getBody();
+
+        assertThat(districtsReturn.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(districtBody).isNotNull();
+        assertThat(districtBody).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Testa quando a lista de bairros tem ao menos um registro")
+    public void getAllDistrictsWithResults() {
         List<District> allDistricts = TestUtil.getAllDistricts();
         String baseUrl = "http://localhost:" + port + "/district";
 
@@ -93,5 +113,48 @@ public class PropertiesIntegrationTest {
         assertThat(districtCreatedBody).isNotNull();
         assertThat(districtCreatedBody[0].getDistrictName()).isEqualTo(newDistrict.getDistrictName());
         assertThat(districtCreatedBody[0].getSquareMeterValue()).isEqualTo(newDistrict.getSquareMeterValue());
+    }
+
+   // properties/register    -> POST new property (precisa criar um distrito antes)
+
+   // properties/calculateSqrFtgProp/1   -> GET Responde com a area total somada de todos os comodos
+
+   // properties/largestRoom/1          -> GET retorna o maior comodo
+
+   // properties/calculatePrice/1       -> GET Calcula o preço total da propriedade (metrosTotal * valorMetro)
+
+   // properties/calculateSqrFtgRoom/1   -> GET area total de cada comodo
+
+
+    @Test
+    @DisplayName("Testa se a rota post funciona para adicionar um imóvel")
+    public void registerAProperty(){
+        String baseUrlDistrict = "http://localhost:" + port + "/district";
+        District newDistrict = TestUtil.newDistrictSaved();
+        HttpEntity<District> httpEntityDistrict = new HttpEntity<>(newDistrict);
+
+        // add o distrito
+        ResponseEntity<District> districtCreatedReturn =
+                testRestTemplate.exchange(baseUrlDistrict + "/registerDistrict",
+                        HttpMethod.POST, httpEntityDistrict, District.class);
+
+
+        // add a propriedade
+        PropertyRequest newProperty = TestUtil.newPropertyRequest();
+        String baseUrl = "http://localhost:" + port + "/properties/register";
+        HttpEntity<PropertyRequest> httpEntity = new HttpEntity<>(newProperty);
+
+
+
+        ResponseEntity<Property> propertyReturn =
+                testRestTemplate.exchange(baseUrl,
+                        HttpMethod.POST, httpEntity, Property.class);
+
+        Property propertyBody = propertyReturn.getBody();
+
+        assertThat(propertyReturn.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(propertyBody).isNotNull();
+
+
     }
 }
